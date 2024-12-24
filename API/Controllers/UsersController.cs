e93fc4801f5fe733893a21a542ee9b307b19c368
mahesh-js-alight <maheshchandiran.js@alight.com>
2024-12-24 17:58:12 +0530

    Add project files.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")] // api/users
+	public class UsersController : ControllerBase
+	{
+		private readonly DataContext dataContext;
+
+		public UsersController(DataContext dataContext)
+		{
+			this.dataContext = dataContext;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersAsync()
+		{
+			var users = await dataContext.Users.ToListAsync();
+			return users;
+		}
+
+		[HttpGet("{id:int}")] // api/users/id
+		public async Task<ActionResult<AppUser>> GetUserAsync(int id)
+		{
+			var user = await dataContext.Users.FindAsync(id);
+			if (user == null) { return NotFound(); }
+			return user;
+		}
+	}
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+		{
+		}
+		public DbSet<AppUser> Users { get; set; }
+	}
+}
